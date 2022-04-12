3472fcb8902778c109b8e83ef1d7c7663161bf0f
Olga <ok2304@yandex.ru>
2022-04-12 18:34:31 +0300

    Commit 1

---
+++ src/test/java/tests/ReqresinTestsDemo.java
@@ -0,0 +1,77 @@
+package tests;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+import static io.restassured.RestAssured.*;
+
+
+
+public class ReqresinTestsDemo {
+
+   // @BeforeAll
+    //static void setUp() {
+      //  RestAssured.baseURI = "https://reqres.in/";
+    //}
+
+
+    @Test
+    void successfulLogin() {
+        /*
+        request: https://reqres.in/api/login
+        data:
+        {
+            "email": "eve.holt@reqres.in",
+            "password": "cityslicka"
+        }
+        response:
+        {
+            "token": "QpwL5tke4Pnpja7X4"
+        }
+         */
+
+        String authorizedData = "{\"email\": \"eve.holt@reqres.in\", " +
+                "\"password\": \"cityslicka\"}";
+
+        given()
+                .body(authorizedData)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void missingPasswordLogin() {
+        /*
+        request: https://reqres.in/api/login
+        data:
+        {
+            "email": "eve.holt@reqres.in"
+        }
+        response:
+        {
+            "error": "Missing password"
+        }
+         */
+
+        String authorizedData = "{\"email\": \"eve.holt@reqres.in\"}";
+
+        given()
+                .body(authorizedData)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login") //post!
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+
+
+}
